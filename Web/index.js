d5b374fbd9f86fbd80152c93f78b36bdb64fb3d7
elemen <124027876+elemen1069@users.noreply.github.com>
2025-01-21 14:40:00 +0900

    NICE :)

--- Web/index.js
+++ Web/index.js
@@ -1,16 +1,23 @@
-const Port = require("../config");
+const Config = require("../config");
 const express = require("express");
-const path = require("path");
 
 const app = express();
 app.use(express.json()); // POST 요청 본문 파싱용 미들웨어
 
+const port = Config.port;
+
 let Status = null;
+let Command = null;
 
 // HTML 파일 제공
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + "/html" + "/index.html"));
+    res.sendFile(`${__dirname}/html/index.html`);
 });
+
+app.get('/log', (req, res) => {
+    console.log(1)
+    res.sendFile(`${__dirname}/html/logs.html`);
+})
 
 // 상태 정보 제공
 app.get('/Status', (req, res) => {
@@ -19,12 +26,28 @@ app.get('/Status', (req, res) => {
 
 // 상태 정보 업데이트 (POST)
 app.post('/updateStatus', (req, res) => {
-    console.log(req.body);
     Status = req.body;
     res.status(200).send("Status updated!");
 });
 
-app.listen(3000, () => {
-    console.log(`${Port}번 포트에서 웹 서버를 시작합니다!`);
-    console.info(`접속하기 : http://localhost:${Port}`);
+app.post('/runCommand', async (req, res) => {
+    const { command } = req.body; // 명령어를 요청에서 가져옴
+
+    if (!command) {
+        return res.status(400).send("No command provided");
+    }
+
+    Command = command;
+});
+
+app.get('/getCommand', (req, res) => {
+
+    res.status(200).send(Command);
+
+    Command = null;
+});
+
+app.listen(port, () => {
+    console.log(`${port}번 포트에서 웹 서버를 시작합니다!`);
+    console.info(`접속하기 : http://localhost:${port}`);
 });
--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 const port = 3000;
 
-module.exports = port;
+const ServerPort = 19132;
+const ServerIp = "121.186.254.249";
+
+module.exports = {
+    port : port,
+
+    ServerPort: ServerPort,
+
+    ServerIp : ServerIp
+}
